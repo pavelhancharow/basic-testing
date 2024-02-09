78c99af30b9a1c757dc85215e4360dfe059d4852
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-02-09 18:42:10 +0100

    fix: add tests to 08-snapshot-testing module

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,63 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const People = {
+  Girl: '👧 - girl',
+  Boy: '👦 - boy',
+  Woman: '👩 - woman',
+  Man: '👨 - man',
+  Lady: '👵 - lady',
+  Senior: '👴 - senior',
+};
+
+const man_array = [People.Boy, People.Man, People.Senior];
+const man_tree = {
+  value: People.Boy,
+  next: {
+    value: People.Man,
+    next: {
+      value: People.Senior,
+      next: { value: null, next: null },
+    },
+  },
+};
+const fail_man_tree = {
+  value: People.Boy,
+  next: {
+    value: People.Woman,
+    next: {
+      value: People.Senior,
+      next: { value: null },
+    },
+  },
+};
+
+const woman_array = [People.Girl, People.Woman, People.Lady];
+const woman_tree = {
+  value: People.Girl,
+  next: {
+    value: People.Woman,
+    next: {
+      value: People.Lady,
+      next: { value: null, next: null },
+    },
+  },
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(man_array)).not.toStrictEqual(fail_man_tree);
+
+    expect(generateLinkedList(man_array)).toStrictEqual(man_tree);
+
+    expect(generateLinkedList([])).not.toStrictEqual(man_tree);
+
+    expect(generateLinkedList([])).toStrictEqual(man_tree.next.next.next);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(woman_array)).toMatchSnapshot(woman_tree);
+    expect(generateLinkedList([])).toMatchSnapshot(woman_tree.next.next.next);
   });
 });
