70e33bf179470a1c1b3741f2e1a9c95565d1ee05
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-02-09 18:36:43 +0100

    fix: add tests to 01-simple-test module

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,37 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+const input = { a: 2, b: 5 };
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: Action.Add })).toBe(7);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: Action.Subtract })).toBe(-3);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: Action.Multiply })).toBe(10);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: Action.Divide })).toBe(0.4);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: Action.Exponentiate })).toBe(
+      32,
+    );
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(simpleCalculator({ ...input, action: '√' })).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({ ...input, b: '0', action: Action.Add }),
+    ).toBeNull();
   });
 });
