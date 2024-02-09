feebfc2e87145e0aec0085b2adb511700343bb5d
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-02-09 18:40:37 +0100

    fix: add tests to 05-partial-mocking module

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,33 @@
-// Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
-jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
-});
+jest.mock('./index', () => ({
+  __esModule: true,
+  ...jest.requireActual<typeof import('./index')>('./index'),
+  mockOne: jest.fn(),
+  mockTwo: jest.fn(),
+  mockThree: jest.fn(),
+}));
+
+const logging = console.log;
+console.log = jest.fn();
 
 describe('partial mocking', () => {
   afterAll(() => {
     jest.unmock('./index');
+    console.log = logging;
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    mockOne();
+    mockTwo();
+    mockThree();
+
+    expect(console.log).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    unmockedFunction();
+
+    expect(console.log).toHaveBeenCalled();
   });
 });
